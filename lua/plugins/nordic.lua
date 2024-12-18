5d73c8ec9214e4d5661a77a7427844f91d285dc5
Jiho Ro <jiho@cloudflare.com>
2024-12-18 14:51:07 -0600

    nordic

---
+++ lua/plugins/nordic.lua
@@ -0,0 +1,14 @@
+return {
+  "AlexvZyl/nordic.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nordic").setup({
+      transparent = {
+        bg = true,
+        float = true,
+      },
+    })
+    require("nordic").load()
+  end,
+}
