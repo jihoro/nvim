0eb1749993d4cde4ee14080d6bcc73d7b7bb4839
Jiho Ro <jihoandy@gmail.com>
2025-01-04 21:54:37 -0600

    get rid of mini.animate and remap <C-d/u>

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -3,8 +3,5 @@ return {
     config = function()
         local statusline = require("mini.statusline")
         statusline.setup({})
-
-        local animate = require("mini.animate")
-        animate.setup({})
     end,
 }
