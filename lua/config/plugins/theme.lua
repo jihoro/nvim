b88b377b1e2aca286c57a88617e713e4d54ef7ee
Jiho Ro <jiho@cloudflare.com>
2025-02-25 19:45:48 -0600

    update colors

---
+++ lua/config/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+    -- 'shaunsingh/nord.nvim',
+    -- config = function()
+    --     vim.g.nord_disable_background = true
+    --     vim.cmd("colorscheme nord")
+    -- end
+    "RRethy/base16-nvim",
+    config = function()
+        vim.cmd("colorscheme base16-black-metal-dark-funeral")
+    end
+}
