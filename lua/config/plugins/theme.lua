257f3f1a65ccfc80214cbf8201b51dce92414545
Jiho Ro <jiho@cloudflare.com>
2025-02-27 12:03:20 -0600

    update plugins

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,11 +1,15 @@
 return {
+    {
+        "RRethy/base16-nvim",
+        config = function()
+            vim.cmd("colorscheme base16-black-metal-dark-funeral")
+        end
+    }
+    -- {
     -- 'shaunsingh/nord.nvim',
     -- config = function()
     --     vim.g.nord_disable_background = true
     --     vim.cmd("colorscheme nord")
     -- end
-    "RRethy/base16-nvim",
-    config = function()
-        vim.cmd("colorscheme base16-black-metal-dark-funeral")
-    end
+    -- }
 }
