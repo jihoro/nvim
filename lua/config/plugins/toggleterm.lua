5e53204b17d913946903e24e34ccd430f95e12dd
Jiho Ro <jiho@cloudflare.com>
2025-03-31 10:48:19 -0500

    change toggle term orientation to horizontal

--- lua/config/plugins/toggleterm.lua
+++ lua/config/plugins/toggleterm.lua
@@ -5,15 +5,21 @@ return {
         config = function()
             require("toggleterm").setup({
                 open_mapping = [[<C-\>]],
-                direction = 'float',
-                size = 40,
+                direction = 'horizontal',
+                size = 20,
                 float_opts = {
                     border = 'rounded',
                 }
             })
             function _G.set_terminal_keymaps()
-                local opts = { noremap = true }
-                vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+                local opts = { buffer = 0 }
+                vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+                vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+                vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+                vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+                vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+                vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+                vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
             end
 
             vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
