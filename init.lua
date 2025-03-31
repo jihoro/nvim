30d9e9949515290fb1deea24fcf6bd3629ce18e1
Jiho Ro <jihoandy@gmail.com>
2025-03-31 09:12:55 -0500

    enable folds + add flash.nvim

--- init.lua
+++ init.lua
@@ -45,3 +45,11 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.g.tmux_navigator_no_mappings = 1
 vim.o.guicursor = ""
+
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
