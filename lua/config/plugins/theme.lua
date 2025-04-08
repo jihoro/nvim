5f2ce7e28a8892d6b4ca03f0c8ff516145273034
Jiho Ro <jiho@cloudflare.com>
2025-04-08 15:32:26 -0500

    black-metal

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
     {
         "RRethy/base16-nvim",
         config = function()
-            vim.cmd("colorscheme base16-black-metal-burzum")
+            vim.cmd("colorscheme base16-black-metal")
             vim.cmd [[
             highlight Normal guibg=NONE ctermbg=NONE
             highlight NormalNC guibg=NONE ctermbg=NONE
